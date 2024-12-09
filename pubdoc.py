828970eb7d2ca79e844195667e5e111cc1cb9f05
Jicai Liu <jicai.liu@mixosense.com>
2024-12-09 14:02:29 +0800

    remove verbose

--- pubdoc.py
+++ pubdoc.py
@@ -1,11 +1,3 @@
-'''
-Author: Jicai Liu jicai.liu@mixosense.com
-Date: 2024-12-09 12:20:09
-LastEditors: Jicai Liu jicai.liu@mixosense.com
-LastEditTime: 2024-12-09 12:20:32
-FilePath: \mxdbg\pubdoc.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 import os
 import os.path
 from pathlib import Path, PureWindowsPath

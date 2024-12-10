a2e95450123ca20ecbfc7c396e569fbfe526dc70
Jicai Liu <jicai.liu@mixosense.com>
2024-12-10 09:49:24 +0800

    remove verbose code

--- src/mxdbg/mxdbg.py
+++ src/mxdbg/mxdbg.py
@@ -11,52 +11,15 @@
 import ctypes
 import os
 import re
-import subprocess
 import sys
 import time
 from types import MappingProxyType
 
-try:
-    from mxdbg.__version__ import __version__
-except ImportError:
-    raise("import mxdbg.__version__ failed.")
-
-try:
-    from mxAlfredHeartbeatAgent import agent
-except ImportError:
-    raise ImportError("Failed to import mxAlfredHeartbeatAgent.")
-
-try:
-    from loguru import logger
-except ImportError:
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "loguru"])
-        from loguru import logger
-    except Exception as e:
-        print(f"Failed to install loguru package: {e}")
-        raise ImportError
-
-try:
-    import toml
-except ImportError:
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "toml"])
-        import toml
-    except Exception as e:
-        logger.error(f"Failed to install toml package: {e}")
-        raise ImportError
-
-try:
-    import serial.tools.list_ports
-    from serial import Serial
-except ImportError:
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "install", "pyserial"])
-        import serial.tools.list_ports
-        from serial import Serial
-    except Exception as e:
-        logger.error(f"Failed to install pyserial package: {e}")
-        raise ImportError
+from mxdbg.__version__ import __version__
+from loguru import logger
+import toml
+import serial.tools.list_ports
+from serial import Serial
 
 class MXDBG:
 
@@ -288,19 +251,6 @@ class MXDBG:
         logger.info("Using ESP32-S3R8.")
         logger.info("Embedded software version: v{}.{}".format(self.version["MAJOR"], self.version["MINOR"]))
         logger.info(f"Library version: {__version__}.")
-                             
-        payload_kpi =  {
-            "timestamp": time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())  # 时间戳
-        }
-        try:
-            agent.send(service_owner="SV",
-                       service_name="mxDBG",
-                       service_description="mxDBG basic service",
-                       period_sec=-1,
-                       kpi=payload_kpi,
-                       debug=True)
-        except ValueError as e:
-            logger.error(e)
 
     def disconnect(self) -> None:
         self.__client.close()
